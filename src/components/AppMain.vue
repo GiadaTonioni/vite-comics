d1a68a85675965cf2e70b69ca938d350f42af7d3
GiadaTonioni <giada.tonioni@gmail.com>
2023-06-20 16:24:31 +0200

    struttura a blocchi main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -8,9 +8,9 @@ export default {
         <main>
             <div class="container"> MAIN
                 <div class="row">
-                    <div class="col">
+                    <div class="col-list">
                         <div class="container">
-                            <div class="row">
+                            <div class="row-list">
                                 <div class="col-small">
                                     <h4>UNO</h4>
                                     <ul>
@@ -32,7 +32,7 @@ export default {
                             </div>
                         </div>
                     </div>
-                    <div class="col">
+                    <div class="col-back">
                         LOGONE
                     </div>
                 </div>
@@ -41,6 +41,38 @@ export default {
     </div>
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss">
 @use '../styles/partials/variables' as*;
+
+.row {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+}
+
+.col-list {
+    border: 1px solid black;
+    height: 400px;
+    width: 40%;
+    background-color: aqua;
+}
+
+.col-back {
+    border: 1px solid black;
+    height: 400px;
+    width: 50%;
+    background-color: rgb(72, 0, 255);
+}
+
+.row-list {
+    display: flex;
+    justify-content: space-evenly;
+    flex-wrap: wrap;
+}
+
+.col-small {
+    height: 300px;
+    width: 20%;
+    background-color: beige;
+}
 </style>
